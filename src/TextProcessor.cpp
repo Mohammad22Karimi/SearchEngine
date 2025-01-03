7d0a7ba73a431bac23057ca7f81b66593f906467
Karimi <mokarimi22.4@gmail.com>
2025-01-03 18:48:43 +0330

    Separation of words

--- src/TextProcessor.cpp
+++ src/TextProcessor.cpp
@@ -14,5 +14,14 @@ vector<string> TextProcessor::clearnText(const string &text)
     string clearnText = text;
     clearnText.erase(remove_if(clearnText.begin(), clearnText.end(), ::ispunct), clearnText.end());
     transform(clearnText.begin(), clearnText.end(), clearnText.begin(), ::tolower);
-    return;
+
+    stringstream ss(clearnText);
+    string word;
+    vector<string> words;
+    while (ss >> word)
+    {
+        words.push_back(word);
+    }
+
+    return words;
 }
