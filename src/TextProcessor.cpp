ed55492598abca11a417f206451893a5dee6e411
Karimi <mokarimi22.4@gmail.com>
2025-01-03 18:08:37 +0330

    add TextProcessor class

---
+++ src/TextProcessor.cpp
@@ -0,0 +1,11 @@
+#include "TextProcessor.h"
+
+TextProcessor::TextProcessor()
+{
+
+}
+
+TextProcessor::~TextProcessor()
+{
+
+}
---
+++ src/TextProcessor.h
@@ -0,0 +1,16 @@
+#ifndef TEXTPROCESSOR_H
+#define TEXTPROCESSOR_H
+
+#pragma once
+
+class TextProcessor
+{
+public:
+    TextProcessor();
+    ~TextProcessor();
+
+private:
+
+};
+
+#endif
