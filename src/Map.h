c620eee97412adefc901f0e4f5727cad77bbd189
Karimi <mokarimi22.4@gmail.com>
2025-01-03 14:51:04 +0330

    add hash function, insert and get function to Map.h

--- src/Map.h
+++ src/Map.h
@@ -2,15 +2,25 @@
 #define MAP_H
 
 #pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
 
 class Map
 {
 public:
     Map();
+    void insert(const string &key, const string &value);
+    string get(const string &key);
     ~Map();
 
 private:
+    static const int SIZE = 100;
+    vector<list<pair<string, string>>> table;
 
+    int hashFunction(const string &key);
 };
 
 #endif
