7ffc7585c070d432e5088aca41425551f124caba
Karimi <mokarimi22.4@gmail.com>
2025-01-03 16:58:37 +0330

    add addDocument, search and tokenize function to InvertedIndex class

--- src/InvertedIndex.h
+++ src/InvertedIndex.h
@@ -2,14 +2,23 @@
 #define INVERTEDINDEX_H
 // ایجاد یک ایندکس که برای هر کلمه، لیستی از اسناد شامل آن کلمه را ذخیره کند
 #pragma once
-
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include "Map.h"
 class InvertedIndex
 {
 public:
     InvertedIndex();
+
+    void addDocument(const string &doc);
+    vector<string> search(const string &query);
     ~InvertedIndex();
 
 private:
+    Map index;
+    vector<string> tokenize(const string &text);
 };
 
 #endif
