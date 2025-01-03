020380af97a31b36089e7d747d4f3715a02c8a76
Karimi <mokarimi22.4@gmail.com>
2025-01-03 18:22:37 +0330

    implement the cleanText for erase the operators and Lowercase all letters

--- src/TextProcessor.cpp
+++ src/TextProcessor.cpp
@@ -1,11 +1,18 @@
 #include "TextProcessor.h"
-
+#include <sstream>
+#include <algorithm>
 TextProcessor::TextProcessor()
 {
-
 }
 
 TextProcessor::~TextProcessor()
 {
+}
 
+vector<string> TextProcessor::clearnText(const string &text)
+{
+    string clearnText = text;
+    clearnText.erase(remove_if(clearnText.begin(), clearnText.end(), ::ispunct), clearnText.end());
+    transform(clearnText.begin(), clearnText.end(), clearnText.begin(), ::tolower);
+    return;
 }
