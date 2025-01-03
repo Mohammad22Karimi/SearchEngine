2d5dd7efdc371a137cbd39dd02bda22835cbb83f
Karimi <mokarimi22.4@gmail.com>
2025-01-03 18:06:01 +0330

    add read Document function and implement it in DocumentReader.cpp

--- src/DocumentReader.cpp
+++ src/DocumentReader.cpp
@@ -1,11 +1,31 @@
 #include "DocumentReader.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace fs = std::filesystem;
 
 DocumentReader::DocumentReader()
 {
+}
 
+vector<pair<string, string>> DocumentReader::readDocument(const string &filePath)
+{
+    vector<pair<string, string>> doc;
+
+    for (auto &entry : fs::directory_iterator(filePath))
+    {
+        if (entry.is_regular_file() && entry.path().extension() == ".txt")
+        {
+            string fileName = entry.path().filename().string();
+            ifstream file(entry.path().string());
+            string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+            doc.emplace_back(fileName, content);
+        }
+    }
+    return vector<pair<string, string>>();
 }
 
 DocumentReader::~DocumentReader()
 {
-
 }
--- src/DocumentReader.h
+++ src/DocumentReader.h
@@ -1,16 +1,20 @@
 #ifndef DOCUMENTREADER_H
 #define DOCUMENTREADER_H
-
+// خواندن فایل‌های متنی از پوشه
 #pragma once
 
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
 class DocumentReader
 {
 public:
     DocumentReader();
+    vector<pair<string, string>> readDocument(const string &filePath);
     ~DocumentReader();
 
 private:
-
 };
 
 #endif
