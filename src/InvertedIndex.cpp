6bc28ddf76a7c89b2ed1066fdd52f6a0cc30c3e3
Karimi <mokarimi22.4@gmail.com>
2025-01-04 11:05:01 +0330

    implement the addDocument fucntion in InvertedIndex.cpp file

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -2,10 +2,43 @@
 
 InvertedIndex::InvertedIndex()
 {
+}
 
+void InvertedIndex::addDocument(const string &doc)
+{
+    ifstream fileP(doc, ios::in);
+
+    if (!fileP)
+    {
+        cerr << "Error opening file: " << doc << endl;
+        return;
+    }
+
+    if (find(fileList.begin(), fileList.end(), doc) != fileList.end())
+    {
+        cerr << "File already indexed: " << doc << endl;
+        return;
+    }
+
+    fileList.push_back(doc);
+    string line;
+
+    TextProcessor processor;
+
+    while (getline(fileP, line))
+    {
+
+        vector<string> words = processor.cleanText(line);
+
+        for (string &word : words)
+        {
+            index.insert(word, doc); // اضافه کردن به ایندکس
+        }
+    }
+
+    fileP.close();
 }
 
 InvertedIndex::~InvertedIndex()
 {
-
 }
