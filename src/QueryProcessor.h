8e955037b2b28db4b7971f6683a1481b5dd88eae
Karimi <mokarimi22.4@gmail.com>
2025-01-05 15:08:42 +0330

    add constructor to QueryProcessor

--- src/QueryProcessor.h
+++ src/QueryProcessor.h
@@ -4,16 +4,20 @@
 #pragma once
 #include <vector>
 #include <string>
+#include "InvertedIndex.h"
 using namespace std;
 class QueryProcessor
 {
 public:
-    QueryProcessor();
+    QueryProcessor(InvertedIndex &ind) : index(ind) {}
     ~QueryProcessor();
 
 private:
+    InvertedIndex index;
+
     class QueryParts
     {
+    public:
         vector<string> mustInclude;
         vector<string> mustExclude;
         vector<string> mustContain;
