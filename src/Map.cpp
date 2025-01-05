50088da626fd746f32237672f346a46d0352e0dd
Karimi <mokarimi22.4@gmail.com>
2025-01-05 13:55:56 +0330

    implement rehash function, and fix the insert function

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,20 +1,32 @@
 #include "Map.h"
 
-Map::~Map()
-{
-}
-
 int Map::hashFunction(const string &key)
 {
-    int hash = 0;
+    size_t hash = 0;
+    size_t prime = 31;
     for (char c : key)
     {
-        hash = (hash * 31 + c) % SIZE;
+        hash = (hash * prime + c) % table.size();
     }
     return hash;
 }
+void Map::rehash()
+{
+    int newSize = table.size() * 2; // اندازه جدید
+    vector<list<pair<string, vector<string>>>> newTable(newSize);
 
-string Map::get(const string &key)
+    for (const auto &bucket : table)
+    {
+        for (const auto &pair : bucket)
+        {
+            int newHashIndex = hashFunction(pair.first) % newSize;
+            newTable[newHashIndex].emplace_back(pair);
+        }
+    }
+
+    table = std::move(newTable); // جایگزینی جدول قدیمی با جدید
+}
+vector<string> Map::get(const string &key)
 {
     int hashIndex = hashFunction(key);
     for (auto &pair : table[hashIndex])
@@ -24,18 +36,29 @@ string Map::get(const string &key)
             return pair.second;
         }
     }
-    return "key not found\n";
+    return {};
 }
 void Map::insert(const string &key, const string &value)
 {
     int hashIndex = hashFunction(key);
+
     for (auto &pair : table[hashIndex])
     {
         if (pair.first == key)
         {
-            pair.second = value;
+            if (find(pair.second.begin(), pair.second.end(), value) == pair.second.end())
+            {
+                pair.second.push_back(value);
+            }
             return;
         }
     }
-    table[hashIndex].emplace_back(key, value);
+
+    table[hashIndex].emplace_back(key, vector<string>{value});
+    currentSize++;
+
+    if (currentSize > table.size() * 0.75)
+    {
+        rehash();
+    }
 }
