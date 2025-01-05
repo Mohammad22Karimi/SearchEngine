b3ba5d5616e28fa08ed457cf5fbdb6763f7f7873
Karimi <mokarimi22.4@gmail.com>
2025-01-05 14:04:11 +0330

    fix the bug in readDocument fucntion

--- src/DocumentReader.cpp
+++ src/DocumentReader.cpp
@@ -5,32 +5,24 @@
 
 namespace fs = std::filesystem;
 
-DocumentReader::DocumentReader()
-{
-}
-
 vector<pair<string, string>> DocumentReader::readDocument(const string &filePath)
 {
     vector<pair<string, string>> doc;
-    InvertedIndex data;
 
     for (const auto &entry : fs::directory_iterator(filePath))
     {
-        if (entry.is_regular_file() && entry.path().extension() == ".txt")
+        if (entry.is_regular_file())
         {
             string filePath = entry.path().string();
             string fileName = entry.path().filename().string();
 
-            data.addDocument(filePath);
+            // InvertedIndex index;
+            // index.addDocument(filePath);
 
             // ذخیره نام فایل فقط (بدون ذخیره محتوا)
-            doc.emplace_back(fileName, "");
+            doc.emplace_back(fileName, filePath);
         }
     }
 
     return doc;
-}
-
-DocumentReader::~DocumentReader()
-{
 }
