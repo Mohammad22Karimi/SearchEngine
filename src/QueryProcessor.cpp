a8958f31107e620ce35fc2ca52e2b52d24da5157
Karimi <mokarimi22.4@gmail.com>
2025-01-05 18:48:49 +0330

    completing the function processQuery

--- src/QueryProcessor.cpp
+++ src/QueryProcessor.cpp
@@ -6,6 +6,54 @@ vector<string> QueryProcessor::processQuery(string &query)
     set<string> mustContainDoc;
     set<string> mustExcludeDoc;
     set<string> mustIncludeDoc;
+
+    if (!qp.mustContain.empty())
+    {
+        mustContainDoc = set<string>(getDocuments(qp.mustContain[0]).begin(),
+                                     getDocuments(qp.mustContain[0]).end());
+        // یافتن اشتراک برای کلمات باقیمانده
+        for (size_t i = 1; i < qp.mustContain.size(); i++)
+        {
+            set<string> docs = getDocuments(qp.mustContain[i]);
+            for (auto it = mustContainDoc.begin(); it != mustContainDoc.end();)
+            {
+                if (docs.find(*it) == docs.end())
+                {
+                    it = mustContainDoc.erase(it);
+                }
+                else
+                {
+                    ++it;
+                }
+            }
+            if (mustContainDoc.empty())
+            {
+                break;
+            }
+        }
+    }
+    for (string &word : qp.mustInclude)
+    {
+        set<string> temp = getDocuments(word);
+        mustIncludeDoc.insert(temp.begin(), temp.end());
+    }
+    for (string &word : qp.mustExclude)
+    {
+        set<string> temp = getDocuments(word);
+        mustExcludeDoc.insert(temp.begin(), temp.end());
+    }
+
+    set<string> result = mustContainDoc;
+
+    if (!mustIncludeDoc.empty())
+    {
+        result.insert(mustIncludeDoc.begin(), mustIncludeDoc.end());
+    }
+    for (const string &doc : mustExcludeDoc)
+    {
+        result.erase(doc);
+    }
+    return vector<string>(result.begin(), result.end());
 }
 
 QueryProcessor::QueryParts QueryProcessor::paresQuery(string &query)
